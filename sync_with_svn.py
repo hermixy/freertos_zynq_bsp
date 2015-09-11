9f2fe4c2eb3a2134a1825e6df3764ba7033c841a
liubenyuan <liubenyuan@gmail.com>
2015-09-11 23:10:36 +0800

    update script with python3 compatible

--- sync_with_svn.py
+++ sync_with_svn.py
@@ -11,35 +11,35 @@ print("sync with FreeRTOS srcs and headers")
 # sync with FREERTOS srcs
 dest_srcs = path.join(dest, r"bsp/freertos_zynq_v8/src/Source/")
 for f in glob.glob(path.join(source, "FreeRTOS/Source/*.c")):
-    print f
+    print(f)
     copy(f, dest_srcs)
 
 dest_srcs = path.join(dest, r"bsp/freertos_zynq_v8/src/Source/include")
 for f in glob.glob(path.join(source, "FreeRTOS/Source/include/*.*")):
-    print f
+    print(f)
     copy(f, dest_srcs)
 
 dest_srcs = path.join(dest, r"bsp/freertos_zynq_v8/src/License")
 f = path.join(source, r"FreeRTOS/License/license.txt")
-print f
+print(f)
 copy(f, dest_srcs)
 
 print("sync with FreeRTOS portable")
 # sync with FREERTOS ports
 dest_srcs = path.join(dest,r"bsp/freertos_zynq_v8/src/Source/portable/")
 f = path.join(source, "FreeRTOS/Source/portable/readme.txt")
-print f
+print(f)
 copy(f, dest_srcs)
 
 dest_srcs = path.join(dest,r"bsp/freertos_zynq_v8/src/Source/portable/GCC/ARM_CA9/")
 for f in glob.glob(path.join(source, "FreeRTOS/Source/portable/GCC/ARM_CA9/*.*")):
-    print f
+    print(f)
     copy(f, dest_srcs)
 
 # sync with Zynq ports
 dest_srcs = path.join(dest,r"bsp/freertos_zynq_v8/src/Source/portable/GCC/Zynq/")
 f = path.join(source,"FreeRTOS/Demo/CORTEX_A9_Zynq_ZC702/RTOSDemo/src/FreeRTOS_asm_vectors.S")
-print f
+print(f)
 copy(f, dest_srcs)
 
 # patch entry point
@@ -56,7 +56,7 @@ with open(fp, "wb") as fid:
 
 #
 f = path.join(source,"FreeRTOS/Demo/CORTEX_A9_Zynq_ZC702/RTOSDemo/src/FreeRTOS_tick_config.c")
-print f
+print(f)
 copy(f, dest_srcs)
 # patch 'extern XScuGic' point
 fp = path.join(dest,r"bsp/freertos_zynq_v8/src/Source/portable/GCC/Zynq/FreeRTOS_tick_config.c")
@@ -69,7 +69,7 @@ with open(fp, "wb") as fid:
         fid.write(line)
 
 f = path.join(source,"FreeRTOS/Demo/CORTEX_A9_Zynq_ZC702/RTOSDemo/src/FreeRTOSConfig.h")
-print f
+print(f)
 copy(f, dest_srcs)
 
 ######## lwIP #########
@@ -79,8 +79,8 @@ dest_srcs = path.join(dest, r'bsp/lwip140/src/lwip-1.4.0/')
 # warning, delete tree before copy !
 if path.exists(dest_srcs):
     rmtree(dest_srcs)
-print "from " + srcs
-print "to " + dest_srcs
+print("from " + srcs)
+print("to " + dest_srcs)
 copytree(srcs, dest_srcs, ignore=ignore_patterns('ports'))
 
 srcs = path.join(source, r'FreeRTOS/Demo/CORTEX_A9_Zynq_ZC702/RTOSDemo/src/lwIP_Demo/lwIP_port')
@@ -88,14 +88,14 @@ dest_srcs = path.join(dest, r'bsp/lwip140/src/contrib/ports/xilinx')
 # warning, delete tree before copy !
 if path.exists(dest_srcs):
     rmtree(dest_srcs)
-print "from " + srcs
-print "to " + dest_srcs
+print("from " + srcs)
+print("to " + dest_srcs)
 copytree(srcs, dest_srcs)
 
 ######## FreeRTOS_CLI ########
 print("Syncing with FreeRTOS CLI")
 dest_srcs = path.join(dest, 'bsp/freertos_cli_v1/src/Source')
 for f in glob.glob(path.join(source, "FreeRTOS-Plus/Source/FreeRTOS-Plus-CLI/*")):
-    print f
+    print(f)
     copy(f, dest_srcs)
 
